068d1fc304093ee99383b7280a97ad3536089fea
clurgen <43728903+clurgen@users.noreply.github.com>
2019-04-03 15:34:10 +0200

    Création du répertoire Test unitaire

---
+++ Ean13.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_unitaire
+{
+    class Ean13
+    {
+        private int[] ean13;
+
+        public Ean13(int[] ean13)
+        {
+            if (ean13.Length !=12)
+            {
+                throw new Exception("Le code Ean 13 ne contient pas 12 entiers");
+            }
+            this.ean13 = new int[13];
+            for (int i = 0; i<12; i++) 
+            {
+                this.ean13[i] = ean13[i];
+            }
+
+            this.ean13 = new int[12];
+            int k = 0;
+            foreach (int i in ean13)
+            {
+                this.ean13[k] = i;
+                k++;
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (this.ean13[i] < 0 || this.ean13[i] > 9) 
+                {
+                    throw new Exception("Les élements du Gencode doivent être compris entre 0 et 9");
+                }
+            }
+        }
+
+        private int poids(int indiceDepart)
+        {
+            int poids = 0;
+            for (int i = indiceDepart; i < 12; i = i + 2)
+            {
+                poids = poids + this.ean13[i];
+            }
+            return poids;
+        }
+
+        public int PoidsImpair()
+        {
+            return this.poids(0);
+        }
+
+        public int PoidsPair()
+        {
+            int poids = this.poids(1) * 3;
+            return poids;
+        }
+
+        public int Reste()
+        {
+            int reste = this.PoidsImpair() + this.PoidsPair();
+            reste = reste % 10;
+            return reste;
+        }
+
+        public int Cle()
+        {
+            int cle = 0;
+            if (this.Reste() == 0) 
+            {
+                cle = 0;
+            }
+            if (this.Reste() != 0 && this.Reste() < 10)
+            {
+                cle = 10 - this.Reste() ;
+            }
+            return cle;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < 12; i++)
+            {
+                if (i % 4 == 0 && i !=0)
+                {
+                    s = s + "-";
+                }
+                s = s + ean13[i];
+
+            }
+            s = s + "-" + this.Cle();
+            return s;
+        }
+
+    }
+}
---
+++ Ean13Test.cs
@@ -0,0 +1,198 @@
+﻿using Test_unitaire;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestUnitaireTest
+{
+    
+    
+    /// <summary>
+    ///Classe de test pour Ean13Test, destinée à contenir tous
+    ///les tests unitaires Ean13Test
+    ///</summary>
+    [TestClass()]
+    public class Ean13Test
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtient ou définit le contexte de test qui fournit
+        ///des informations sur la série de tests active ainsi que ses fonctionnalités.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Attributs de tests supplémentaires
+        // 
+        //Vous pouvez utiliser les attributs supplémentaires suivants lorsque vous écrivez vos tests :
+        //
+        //Utilisez ClassInitialize pour exécuter du code avant d'exécuter le premier test dans la classe
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Utilisez ClassCleanup pour exécuter du code après que tous les tests ont été exécutés dans une classe
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Utilisez TestInitialize pour exécuter du code avant d'exécuter chaque test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Utilisez TestCleanup pour exécuter du code après que chaque test a été exécuté
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Test pour Constructeur Ean13
+        ///</summary>
+        [TestMethod()]
+        public void Ean13ConstructorTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0,0,0,0,0,0,0,0,0,0,0,9 });
+            //Assert.Inconclusive("TODO: implémentez le code pour vérifier la cible");
+        }
+
+        /// <summary>
+        ///Test pour Constructeur Ean13
+        ///</summary>
+        [TestMethod()]
+        public void Ean13ConstructorTest0a9()
+        {
+            try
+            {
+                Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,9 });
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Les élements du Gencode doivent être compris entre 0 et 9");
+                return;
+            }
+            Assert.Fail("Les élements du Gencode sont compris entre 0 et 9");
+        }
+
+        /// <summary>
+        ///Test pour Constructeur Ean13
+        ///</summary>
+        [TestMethod()]
+        public void Ean13ConstructorTest_13()
+        {
+            try
+            {
+                Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 });
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Le code Ean 13 ne contient pas 12 entiers");
+                return;
+            }
+            Assert.Fail("Le code Ean 13 contient 12 entiers");
+        }
+        /// <summary>
+        ///Test pour Constructeur Ean13
+        ///</summary>
+        [TestMethod()]
+        public void Ean13ConstructorTest_11()
+        {
+            try
+            {
+                Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Le code Ean 13 ne contient pas 12 entiers");
+               return;
+            }
+            Assert.Fail("Le code Ean 13 contient 12 entiers");
+        }
+        /// <summary>
+        ///Test pour PoidsImpair
+        ///</summary>
+        [TestMethod()]
+        public void PoidsImpairTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }); // TODO: initialisez à une valeur appropriée
+            int expected = 0; // TODO: initialisez à une valeur appropriée
+            int actual;
+            actual = target.PoidsImpair();
+            Assert.AreEqual(expected, actual);
+            //Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+
+        /// <summary>
+        ///Test pour PoidsPair
+        ///</summary>
+        [TestMethod()]
+        public void PoidsPairTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }); // TODO: initialisez à une valeur appropriée
+            int expected = 0; // TODO: initialisez à une valeur appropriée
+            int actual;
+            actual = target.PoidsPair();
+            Assert.AreEqual(expected, actual);
+           // Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+
+        /// <summary>
+        ///Test pour Reste
+        ///</summary>
+        [TestMethod()]
+        public void ResteTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }); // TODO: initialisez à une valeur appropriée
+            int expected = 0; // TODO: initialisez à une valeur appropriée
+            int actual;
+            actual = target.Reste();
+            Assert.AreEqual(expected, actual);
+            // Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+
+        /// <summary>
+        ///Test pour Cle
+        ///</summary>
+        [TestMethod()]
+        public void CleTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }); // TODO: initialisez à une valeur appropriée
+            int expected = 0; // TODO: initialisez à une valeur appropriée
+            int actual;
+            actual = target.Cle();
+            Assert.AreEqual(expected, actual);
+            // Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+
+        /// <summary>
+        ///Test pour ToString
+        ///</summary>
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Ean13 target = new Ean13(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }); // TODO: initialisez à une valeur appropriée
+            string expected = "0000-0000-0000-0"; // TODO: initialisez à une valeur appropriée
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+            // Assert.Inconclusive("Vérifiez l\'exactitude de cette méthode de test.");
+        }
+    }
+}
